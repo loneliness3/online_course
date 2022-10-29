8ea3d37163e3edd8e777a0855f3acc9d277adb1b
loneliness3 <lone32573@gmail.com>
2022-10-29 20:08:49 +0630

    5th try

--- app.py
+++ app.py
@@ -76,7 +76,11 @@ api.add_resource(CourseLists, "/courses/all")
 #delete category
 api.add_resource(DeleteCategory, "/categories/delete")
 
+@app.before_first_request
+def create_tables():
+    db.create_all()
+
 if __name__ == "__main__":
-    # from db import db
-    # db.init_app(app)
+    from db import db
+    db.init_app(app)
     app.run(port=5000, debug= True)
